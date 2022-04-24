74684a6360e27965279e83a2f87e8f2ed74d4c98
Josh Nies <josh.nies@hey.com>
2022-04-24 15:16:22 -0400

    create project path on init

--- cmd/projects.go
+++ cmd/projects.go
@@ -1,11 +1,31 @@
 package cmd
 
 import (
+	"log"
+	"os"
+	"path/filepath"
+
 	"github.com/urfave/cli/v2"
 )
 
 func Init(c *cli.Context) error {
-	// TODO: Get absolute file path
+	// Get absolute file path
+	path := c.Args().First()
+	if path == "" {
+		path = "."
+	}
+
+	absPath, err := filepath.Abs(path)
+	if err != nil {
+		return err
+	}
+
+	// Create directories if they don't exist
+	err = os.MkdirAll(absPath, os.ModePerm)
+	if err != nil {
+		log.Fatalf("\"%s\" is an existing file, aborting...", absPath)
+	}
+
 	// TODO: Create project in API
 	// TODO: Create QC project file
 	// TODO: Create QC history file
