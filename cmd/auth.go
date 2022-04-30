b2d83fca0596302302e6ce50b207a733ccf02146
Josh Nies <josh.nies@hey.com>
2022-04-30 15:07:21 -0400

    basis for cli auth

--- cmd/auth.go
+++ cmd/auth.go
@@ -1,13 +1,28 @@
 package cmd
 
 import (
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/joshnies/qc-cli/lib/console"
 	"github.com/urfave/cli/v2"
 )
 
 // Log in to Quanta Control.
 func LogIn(c *cli.Context) error {
-	// TODO: Log in via Magic Link
+	console.Info("Opening browser to log in to Quanta Control...")
 
-	println("TODO")
-	return nil
+	// Start local HTTP server for receiving Auth0 authentication callback requests
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// TODO: Handle callback request
+		// Reference: https://www.altostra.com/blog/cli-authentication-with-auth0
+		console.Info("Received authentication callback request")
+		os.Exit(0)
+	})
+	go http.ListenAndServe(":4242", nil)
+
+	// Timeout after 3 minutes
+	time.Sleep(time.Second * 180)
+	return console.Error("Ending authentication process after 3 minutes")
 }
