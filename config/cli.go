ea507b575e6402b67b954dc66e47c28ab7b2afb2
Josh Nies <josh.nies@hey.com>
2022-05-06 10:47:28 -0400

    update cli config

--- config/cli.go
+++ config/cli.go
@@ -3,25 +3,34 @@ package config
 import "os"
 
 type APIConfig struct {
+	// API hostname.
 	Host string
 }
 
 type StorageConfig struct {
+	// Bucket name.
 	Bucket string
 }
 
 type CLIConfig struct {
+	// Whether or not to run in sandbox mode.
+	Sandbox bool
+	// Whether or not to print verbose output.
 	Verbose bool
-	API     APIConfig
+	// API configuration.
+	API APIConfig
+	// Storage configuration.
 	Storage StorageConfig
 }
 
+// Singleton CLI config instance.
 var I CLIConfig
 
 // Initialize the CLI config.
 func InitConfig() CLIConfig {
-	// TODO: Update to production values once ready, with env var overrides for testing
 	I = CLIConfig{
+		// TODO: Implement sandbox mode.
+		Sandbox: os.Getenv("SANDBOX") == "1",
 		Verbose: os.Getenv("VERBOSE") == "1",
 		API: APIConfig{
 			Host: "http://localhost:8080/v1",
