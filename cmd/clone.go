7a75347ebb6fa9205e4cc3f14bb74c5a581d6dcd
Josh Nies <josh.nies@hey.com>
2022-05-15 17:09:14 -0400

    clone cmd

--- cmd/clone.go
+++ cmd/clone.go
@@ -3,36 +3,50 @@ package cmd
 import (
 	"encoding/json"
 	"errors"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 
+	"github.com/joshnies/qc-cli/config"
 	"github.com/joshnies/qc-cli/constants"
 	"github.com/joshnies/qc-cli/lib/api"
 	"github.com/joshnies/qc-cli/lib/auth"
 	"github.com/joshnies/qc-cli/lib/console"
 	"github.com/joshnies/qc-cli/lib/httpw"
+	"github.com/joshnies/qc-cli/lib/storj"
 	"github.com/joshnies/qc-cli/models"
 	"github.com/urfave/cli/v2"
+	"golang.org/x/exp/maps"
 )
 
 // Clone remote project at default branch to local machine.
-// TODO: Clone projects by name
-// TODO: Scope projects to a team or user
 func CloneProject(c *cli.Context) error {
 	gc := auth.Validate()
 
-	// Get project ID
-	projectID := c.Args().First()
-	if projectID == "" {
-		return console.Error("Please provide a project ID")
+	// Get project blob from first arg
+	projectBlob := c.Args().First()
+	if projectBlob == "" {
+		return console.Error("Please specify a project in the format of \"<author_or_team_alias>/<project_name>\"")
+	}
+
+	// Get clone path from second arg
+	clonePath := c.Args().Get(1)
+	if clonePath == "" {
+		clonePath = "."
+	}
+	clonePath, err := filepath.Abs(clonePath)
+	if err != nil {
+		return console.Error("Invalid project path")
 	}
 
 	// Check if already in project directory
 	if _, err := os.Stat(constants.ProjectFileName); errors.Is(err, os.ErrNotExist) {
-		return console.Error("Project already exists in current directory")
+		return console.Error("A project already exists in the current directory")
 	}
 
 	// Get project
-	apiUrl := api.BuildURLf("projects/%s")
+	// TODO: Implement this API route
+	apiUrl := api.BuildURLf("projects/blob/%s", projectBlob)
 	res, err := httpw.Get(apiUrl, gc.Auth.AccessToken)
 	if err != nil {
 		return err
@@ -46,23 +60,60 @@ func CloneProject(c *cli.Context) error {
 	}
 
 	// Get default branch
-	apiUrl = api.BuildURLf("projects/%s/branches/default")
+	apiUrl = api.BuildURLf("projects/%s/branches/default?join_commits=true", project.ID)
 	res, err = httpw.Get(apiUrl, gc.Auth.AccessToken)
 	if err != nil {
 		return err
 	}
 
 	// Parse default branch
-	var branch models.Branch
+	var branch models.BranchWithCommit
 	err = json.NewDecoder(res.Body).Decode(&branch)
 	if err != nil {
 		return err
 	}
 
-	console.Info("Cloning project...")
+	console.Info("Cloning project \"%s\" into \"%s\"...", project.Name, clonePath)
 
-	// TODO: Create project config file
-	// TODO: Download all files
+	// Create project config file
+	projectConfig := models.ProjectConfig{
+		ProjectID:          project.ID,
+		CurrentBranchID:    branch.ID,
+		CurrentCommitIndex: branch.Commit.Index,
+	}
+
+	projectConfig, err = config.SaveProjectConfig(filepath.Join(clonePath, constants.ProjectFileName), projectConfig)
+	if err != nil {
+		return err
+	}
+
+	// Download all files
+	dataMap, err := storj.DownloadBulk(projectConfig.ProjectID, maps.Values(branch.Commit.HashMap))
+	if err != nil {
+		return err
+	}
+
+	// Iterate over downloaded files and write each to disk
+	for _, hash := range maps.Keys(dataMap) {
+		// Get file path
+		var path string
+		for p, h := range branch.Commit.HashMap {
+			if hash == h {
+				path = p
+				break
+			}
+		}
+
+		if path == "" {
+			return console.Error("Failed to download file with hash %s", hash)
+		}
+
+		// Write file to local filesystem
+		err = ioutil.WriteFile(path, dataMap[hash], 0644)
+		if err != nil {
+			return console.Error("Failed to write file (%s) after downloading: %s", path, err)
+		}
+	}
 
 	return nil
 }
