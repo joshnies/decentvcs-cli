aaa51346e3686d50251d2868cd6f9ed163b249af
Josh Nies <josh.nies@hey.com>
2022-06-08 11:28:10 -0400

    add dvcs alias to install script

--- install.sh
+++ install.sh
@@ -8,3 +8,27 @@ fi
 
 # Install CLI
 go build && go install
+
+echo "Installed"
+
+# Add alias if not set
+if [[ $SHELL = "/bin/zsh" ]]; then
+  # zsh
+  shell_file="$HOME/.zshrc"
+elif [[ $SHELL = "/bin/bash" ]]; then
+  # bash
+  if test -f ~/.profile; then
+    shell_file="$HOME/.profile"
+  else
+    shell_file="$HOME/.bash_profile"
+  fi
+else
+  echo "Warning: Unknown shell $SHELL; aliases were not added"
+  exit 0
+fi
+
+if ! grep -q "alias dvcs" $shell_file; then
+  echo 'alias dvcs="decent vcs"' >> $shell_file
+  echo "Added the following aliases to $shell_file:"
+  echo '  alias dvcs="decent vcs"'
+fi
