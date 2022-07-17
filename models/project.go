7843b52ac8aec44cea1a03edbe1b16b3a3a36d07
Josh Nies <josh.nies@hey.com>
2022-07-17 08:37:15 -0400

    update models

--- models/project.go
+++ models/project.go
@@ -1,20 +1,20 @@
 package models
 
+import "time"
+
 type Project struct {
-	ID              string   `json:"_id,omitempty"`
-	CreatedAt       int64    `json:"created_at,omitempty"`
-	Name            string   `json:"name,omitempty"`
-	Blob            string   `json:"blob,omitempty"`
-	TeamID          string   `json:"team_id,omitempty"`
-	Branches        []Branch `json:"branches,omitempty"`
-	DefaultBranchID string   `json:"default_branch_id,omitempty"`
+	ID              string    `json:"_id,omitempty"`
+	CreatedAt       time.Time `json:"created_at,omitempty"`
+	Name            string    `json:"name,omitempty"`
+	TeamID          string    `json:"team_id,omitempty"`
+	Branches        []Branch  `json:"branches,omitempty"`
+	DefaultBranchID string    `json:"default_branch_id,omitempty"`
 }
 
 type ProjectWithBranchesAndCommit struct {
 	ID              string             `json:"_id,omitempty"`
-	CreatedAt       int64              `json:"created_at,omitempty"`
+	CreatedAt       time.Time          `json:"created_at,omitempty"`
 	Name            string             `json:"name,omitempty"`
-	Blob            string             `json:"blob,omitempty"`
 	TeamID          string             `json:"team_id,omitempty"`
 	Branches        []BranchWithCommit `json:"branches,omitempty"`
 	DefaultBranchID string             `json:"default_branch_id,omitempty"`
