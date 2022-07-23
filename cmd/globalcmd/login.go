a77bad49cb9bd5a211e1185dd6717960c1521e61
Josh Nies <josh.nies@hey.com>
2022-07-23 11:13:47 -0400

    rm auth call from login cmd

--- cmd/globalcmd/login.go
+++ cmd/globalcmd/login.go
@@ -1,8 +1,6 @@
 package globalcmd
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -12,9 +10,7 @@ import (
 
 	"github.com/joshnies/decent/config"
 	"github.com/joshnies/decent/lib/console"
-	"github.com/joshnies/decent/lib/httpvalidation"
 	"github.com/joshnies/decent/lib/system"
-	"github.com/joshnies/decent/models"
 	"github.com/urfave/cli/v2"
 	"gopkg.in/yaml.v3"
 )
@@ -25,7 +21,7 @@ func LogIn(c *cli.Context) error {
 	// NOTE: Only OAuth (social login) is supported for the CLI due to PKCE.
 	port := 4242
 	redirectUri := url.QueryEscape(fmt.Sprintf("http://localhost:%d", port))
-	authUrl := fmt.Sprintf("%s/login?require=true&require_oauth=true&redirect_uri=%s", config.I.WebsiteURL, redirectUri)
+	authUrl := fmt.Sprintf("%s/login?redirect_uri=%s", config.I.WebsiteURL, redirectUri)
 	console.Info("Opening browser to log you in...")
 	console.Info("You can also open this URL:")
 	fmt.Println(authUrl + "\n")
@@ -36,55 +32,16 @@ func LogIn(c *cli.Context) error {
 		console.Verbose("Received auth redirect")
 
 		// Get token (not the session token!) from query params
-		token := r.URL.Query().Get("token")
-		if token == "" {
+		sessionToken := r.URL.Query().Get("session_token")
+		if sessionToken == "" {
 			// User was most likely redirected after successful login, silently exit with non-zero exit code
 			os.Exit(0)
 		}
-		console.Verbose("Token: %s", token)
-
-		tokenType := r.URL.Query().Get("stytch_token_type")
-		if tokenType == "" {
-			// User was most likely redirected after successful login, silently exit with non-zero exit code
-			os.Exit(0)
-		}
-		console.Verbose("Token type: %s", tokenType)
-
-		// Authenticate with DecentVCS server
-		console.Verbose("Authenticating with DecentVCS server...")
-		httpClient := http.Client{}
-		reqUrl := config.I.VCS.ServerHost + "/authenticate"
-		reqBody := models.AuthenticateRequest{
-			Token:     token,
-			TokenType: tokenType,
-		}
-		reqBodyJson, _ := json.Marshal(reqBody)
-		req, _ := http.NewRequest("POST", reqUrl, bytes.NewBuffer(reqBodyJson))
-		req.Header.Set("Content-Type", "application/json")
-		res, err := httpClient.Do(req)
-		if err != nil {
-			console.Fatal("Failed to authenticate with DecentVCS server: %s", err.Error())
-		}
-		if err = httpvalidation.ValidateResponse(res); err != nil {
-			console.Fatal("Failed to authenticate with DecentVCS server: %s", err.Error())
-		}
-		defer res.Body.Close()
-
-		// Parse authentication response
-		console.Verbose("Parsing DecentVCS server authentication response...")
-		var authRes models.AuthenticateResponse
-		err = json.NewDecoder(res.Body).Decode(&authRes)
-		if err != nil {
-			console.Fatal("Failed to parse DecentVCS server authentication response: %s", err.Error())
-		}
-
-		if authRes.SessionToken == "" {
-			console.Fatal("Failed to authenticate with DecentVCS server: no session token returned")
-		}
+		console.Verbose("Session token: %s", sessionToken)
 
 		// Update config with auth data
 		console.Verbose("Updating config file with new session...")
-		config.I.Auth.SessionToken = authRes.SessionToken
+		config.I.Auth.SessionToken = sessionToken
 
 		newConfig := config.I
 		config.OmitInternalConfig(&newConfig)
