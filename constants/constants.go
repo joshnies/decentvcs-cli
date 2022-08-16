b171e372ffa30554446004e867560a5a1dbf7886
Josh Nies <josh.nies@hey.com>
2022-08-16 09:09:43 -0400

    simplify examples dir

--- constants/constants.go
+++ constants/constants.go
@@ -21,3 +21,13 @@ const SlowDownFileContents = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Error><
 
 // zstd
 const ZstdHeader = "28b52ffd"
+
+// VCS server
+const SessionTokenHeader = "X-Session-Token"
+
+// File system
+const ProjectFileName = ".decent"
+const IgnoreFileName = ".decentignore"
+
+// Error messages
+const ErrNoProject = "Looks like you're not in a DecentVCS project. You can use `dvcs init` to create one."
